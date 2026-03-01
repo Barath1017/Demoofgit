d7c9eb6fad7b02993ce83f1b477dc08d34843550
Barath <barathg941@gmail.com>
2026-03-01 16:48:32 +0530

    This is initial commit

---
+++ index.js
@@ -0,0 +1,29 @@
+console.log("Welcome to the git course");
+
+/* PS C:\Users\barath\OneDrive\OneDrive\Music\Git> git config --global init.defaultBranch main
+PS C:\Users\barath\OneDrive\OneDrive\Music\Git> git init
+Reinitialized existing Git repository in C:/Users/barath/OneDrive/OneDrive/Music/Git/.git/
+PS C:\Users\barath\OneDrive\OneDrive\Music\Git> git status
+On branch main
+
+No commits yet
+
+Untracked files:
+  (use "git add <file>..." to include in what will be committed)
+        index.html
+        index.js
+        readme.md
+
+nothing added to commit but untracked files present (use "git add" to track)
+PS C:\Users\barath\OneDrive\OneDrive\Music\Git> git add readme.md 
+PS C:\Users\barath\OneDrive\OneDrive\Music\Git> git commit -m 'readme'
+[main (root-commit) 5a0fea4] readme
+ 1 file changed, 2 insertions(+)
+ create mode 100644 readme.md
+PS C:\Users\barath\OneDrive\OneDrive\Music\Git> git log
+commit 5a0fea43ae7f512bae9b3a2febb2c964d4278af0 (HEAD -> main)
+Author: Barath <barathg941@gmail.com>
+Date:   Sat Nov 15 08:15:59 2025 +0530
+
+    readme
+PS C:\Users\barath\OneDrive\OneDrive\Music\Git>  */
